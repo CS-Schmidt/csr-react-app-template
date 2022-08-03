b6eaf2f4065d906d91ce799bbff46e188d436097
ES-Schmidt <ethan.schmidt.ab@gmail.com>
2022-08-03 15:41:56 -0600

    feat(core): add react app template files

---
+++ src/app/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/index.css';
+import reactLogo from '../assets/logo.svg';
+
+export default function App() {
+	return (
+		<div>
+			<img src={reactLogo} alt="react logo" />
+			<h1>csr react app template</h1>
+		</div>
+	);
+}
